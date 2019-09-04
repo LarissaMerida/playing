e9fc179940124c11efed25237aa0d8beabdf5934
Larissa <larisadasilvasantos746@gmail.com>
2019-09-04 18:50:57 -0300

    mexendo com mapas

---
+++ teste/teste/maps/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url                                                                                                                              
+from . import views
+
+urlpatterns = [ 
+    url(r'mapa1/', views.default_map, name="default"),
+    url(r'mapa2/', views.default_map2, name="default2"),
+]
---
+++ teste/teste/maps/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def default_map(request):
+    return render(request, 'default.html', {})
+
+
+
+def default_map2(request):
+    return render(request, 'mapa2.html', {})
